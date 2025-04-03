45cbd0beaaf25007f8d60bfdb8cf0cca36b113a0
charlesms1246 <charlesms2006@gmail.com>
2025-04-03 12:50:00 +0530

    fix error

--- ServerCode/telegram.py
+++
@@ -1,3 +0,0 @@
-import telebot
-bot=telebot.TeleBot("7936818721:AAF6tSnhOpGvotjDf9n22IluKPxWWy2Wwto") # Replace with your actual bot token
-bot.send_message(5483279927,"!!BOT IS UNDER DEVELOPMENT!!")
